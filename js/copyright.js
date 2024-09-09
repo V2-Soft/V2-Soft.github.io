9c87c90f911ddc6256772a2708471e795efad403
v2soft <110644430+V2-Soft@users.noreply.github.com>
2024-09-09 17:07:16 +0800

    Create copyright.js

---
+++ js/copyright.js
@@ -0,0 +1,31 @@
+document.write(
+'<div class="footer">'+
+'<ul class="bottom-nav">'+
+'<li>'+
+'<img src="./img/logo2.png" alt="logo" width="200px" class="bottom-logo" draggable="false"/>'+
+'</li>'+
+'<li>'+
+'<ul class="bottom-nav-items-right-big">'+
+'<li>使用V2引擎时请遵守您所在国家的相关法律法规。我们不推荐，也不建议您将我们的软件用于游戏运营、经营等行为。</li>'+
+// '<li>222</li>'+
+'</ul>'+
+'<ul class="bottom-nav-items-right-big">'+
+'<li>任何组织或个人因使用V2游戏引擎引发法律后果的，相关后果由其行为人自行承担，与V2游戏引擎及引擎作者无关！</li>'+
+// '<li>222</li>'+
+'</ul>'+
+'<ul class="bottom-nav-items-right-big">'+
+'<li>V2引擎无法对用户的行为进行检测，任何用户的行为均不代表V2Soft，且相关行为不受到V2Soft的支持。</li>'+
+// '<li>222</li>'+
+'</ul>'+
+'</li>'+
+'</ul>'+
+
+// '<div class="otherinfo" align="center">'+
+// '友情链接: &nbsp;<a href="http://43.249.194.34:12345" target="_blank">13142SF</a>'+
+// '<a href="https://www.lomdn.com/" target="_blank">LOMDN</a>'+
+// '<a href="http://www.aspm2.com/" target="_blank">ASPM2</a>'+
+// '</div>'+
+
+'<div class="copyright" align="center">Copyrighted © 2022 - 2024 www.v2m2.top. All Rights Reserved.</div>'+
+'</div>'
+)
