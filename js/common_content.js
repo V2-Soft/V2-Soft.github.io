394fc15fd5c3e8824df213a59efa3165d3dfbeff
v2soft <110644430+V2-Soft@users.noreply.github.com>
2024-09-09 17:07:01 +0800

    Create common_content.js

---
+++ js/common_content.js
@@ -0,0 +1,4 @@
+document.write(
+'<script type="text/javascript" src="./js/usertoknow.js" charset="UTF-8"></script>'+
+'<script type="text/javascript" src="./js/a1d.js" charset="UTF-8"></script>'
+)
