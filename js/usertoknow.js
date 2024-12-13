709152e51af33fd09ab2d8f5300e97940ea2e9b8
v2soft <110644430+V2-Soft@users.noreply.github.com>
2024-12-13 23:06:06 +0800

    Add files via upload

--- js/usertoknow.js
+++ js/usertoknow.js
@@ -1,5 +1,25 @@
 document.write(
 '<div class="need_know">'+
+'<h3>严正声明</h1>' +
+'<br>'+
+'<p>'+
+'	　　<strong style="color: red;">关于近期网络上出现所谓“出售V2引擎源码”的组织、网站以及个人。</strong>'+
+'</p>'+
+'<p>'+
+'	　　<strong style="color: red;">V2Soft及V2M2在此做出正式声明，任何使用所谓“V2M2源码”进行二次开发、商业运营等行为的，与V2Soft、V2M2及V2系列软件作者无关。与此同时，任何买卖相关源码的个人及组织，与V2Soft无关。</strong>' + 
+'</p>'+
+'<p>'+
+'	　　<strong style="color: red;">同时正告各购买了所谓“V2M2源码”的买家：相关源码不受V2Soft支持、不被V2Soft承认、V2Soft不保证其真实性、V2Soft不为其真实性负责、V2Soft不为其可用性负责、V2Soft不为其出现的问题负责。</strong>' + 
+'</p>'+
+'<p>'+
+'	　　<strong style="color: red;">请各位用户下载软件时认准唯一官网（www.v2m2.top），任何其余渠道V2Soft均不保证软件的安全性、可用性。</strong>' + 
+'</p>'+
+'<br>'+
+'<p>'+
+'	　　<strong style="color: red;">同时V2Soft要做出辟谣，V2Soft所出品的V2系列软件从未标榜过“史上最强、超越XX引擎、无限仿XX引擎”等名头，任何相关描述均属于虚假宣传及欺骗行为，请各位用户注意甄别。由相关描述所引起的问题，与V2Soft、V2M2及V2系列软件作者无关。</strong>' + 
+'</p>'+
+'</div>' +
+'<div class="need_know">'+
 '<h3>用户须知</h3>'+
 '<br>'+
 '<p>　　本站所出品软件是<strong>完全免费</strong>，作为个人学习研究及单机娱乐为目的，使用前请务必认真阅读和理解《下载须知》中规定的所有权利和限制。</p>'+
